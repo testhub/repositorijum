a2654d62bcd4ce3cad01d86974194db12291040e
Test Hub <guglposhta@gmail.com>
2012-01-04 16:51:39 +0100

    Added Second Java Class

--- NewJavaProject/src/newJavaProject/SecondJavaClass.java
+++ NewJavaProject/src/newJavaProject/SecondJavaClass.java
@@ -1,12 +1,13 @@
 package newJavaProject;
 
-public class NewJavaClass {
+public class SecondJavaClass {
 
 	/**
 	 * @param args
 	 */
+	static int dusan=112121; 
 	public static void main(String[] args) {
-		System.out.println(SecondJavaClass.dusan);
+	System.out.println("SecondJavaClass");
 
 	}
 
